a1734bc863e6ed777912c2a9046668b5c2510356
DreamWillBeReal <120799088+DarkHorseCorder@users.noreply.github.com>
2021-12-26 21:31:56 -0800

    add environment

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -87,7 +87,6 @@ export class AuthSignInComponent implements OnInit
                         // to the correct page after a successful sign in. This way, that url can be set via
                         // routing file and we don't have to touch here.
                         const redirectURL = this._activatedRoute.snapshot.queryParamMap.get('redirectURL') || '/signed-in-redirect';
-
                         // Navigate to the redirect url
                         this._router.navigateByUrl(redirectURL);
                     }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// export const environment = {
+//     production: true,
+//     apiEndPoint: 'http://localhost:8080/web/api/coreapi',
+//     fileUploadURL: 'http://localhost:8080/web/dta/dms/',
+// };
+
+export const environment = {
+    production: true,
+    apiEndPoint: 'https://app.tinman-asia.com'
+    };
